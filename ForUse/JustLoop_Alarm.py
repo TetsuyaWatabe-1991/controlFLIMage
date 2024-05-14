a4fd39fe62ef887bffa29dc3210461826e1cb0be
TetsuyaWatabe <104693389+TetsuyaWatabe-1991@users.noreply.github.com>
2024-05-13 21:50:22 -0400

    add files via upload
    just loop alarm and z only DriftCorrection

---
+++ ForUse/JustLoop_Alarm.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  3 12:28:44 2023
+
+@author: yasudalab
+"""
+# import sys
+# sys.path.append("../")
+#%%
+import time
+from controlflimage_threading import control_flimage
+from send_line import line_notification
+import winsound
+
+
+# while True:
+#     winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
+#     yn = input("Could you hear sound?  (y/n) ")
+#     if yn == "y":
+#         break
+
+
+ini_path = r'C:\Users\Yasudalab\Documents\Tetsuya_GIT\controlFLIMage\DirectionSetting.ini'
+
+FLIMageCont = control_flimage(ini_path=ini_path)
+FLIMageCont.directionMotorY = FLIMageCont.directionMotorY 
+
+# FLIMageCont.set_param(RepeatNum=80, interval_sec=60, ch_1or2=2,
+#                       LoadSetting=False,drift_control=True,
+#                       ShowUncagingDetection=True,drift_cont_galvo=False,expected_grab_duration_sec=40) 
+repeat_times = 6
+
+interval_sec = 60
+ch_1or2 = 1
+expected_grab_duration_sec=15
+
+FLIMageCont.set_param(RepeatNum=repeat_times, interval_sec=interval_sec, ch_1or2=ch_1or2,
+                      LoadSetting=False,drift_control=True, 
+                      drift_cont_XY =  False,
+                      drift_cont_galvo=False,expected_grab_duration_sec=expected_grab_duration_sec)       
+FLIMageCont.start_repeat()
+
+line_notification(message = f"{repeat_times} loop  finished")
+
+for i in range(1):
+    winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
+
+
+# %%
